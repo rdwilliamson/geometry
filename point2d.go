6466ec0094cfff1a47f26009f4a9a7e5bf8c8e24
Robert Williamson <williamson.robert@gmail.com>
2011-09-13 04:54:55 -0500

    Some convenience functions.

--- point2d.go
+++ point2d.go
@@ -8,6 +8,18 @@ type Point2D struct {
 	X, Y float64
 }
 
+func (p1 Point2D) Plus(p2 Point2D) Point2D {
+	return Point2D{p1.X+p2.X, p1.Y+p2.Y}
+}
+
 func (p Point2D) String() string {
 	return fmt.Sprintf("(%g, %g)", p.X, p.Y)
+}
+
+func (p1 Point2D) Minus(p2 Point2D) Point2D {
+	return Point2D{p1.X-p2.X, p1.Y-p2.Y}
+}
+
+func (p Point2D) Times(s float64) Point2D {
+	return Point2D{p.X*s, p.Y*s}
 }
