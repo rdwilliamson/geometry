e1013e0424348f72b9476279535614500245a324
Robert Williamson <williamson.robert@gmail.com>
2012-07-15 13:09:34 -0500

    Cleaned up a bit.

--- geometry.go
+++ geometry.go
@@ -1,7 +1,6 @@
-// Geometry package implementing points, vectors, lines, planes, and triangles.
+// Package geometry contains gemoetric primitives.
 //
-// Angles are in radians with zero being the positive x axis and positive
-// corresponding to the counter-clockwise direction.
+// Angles are in radians.
 package geometry
 
 import (
@@ -11,21 +10,4 @@ import (
 // Check if a and b are very close.
 func FuzzyEqual(a, b float64) bool {
 	return math.Abs(a-b) <= 0.000000000001*math.Min(math.Abs(a), math.Abs(b))
-}
-
-// Clamp a such that it is at least min and no more than max.
-func Clamp(a, min, max float64) float64 {
-	if a < min {
-		return min
-	}
-	if a > max {
-		return max
-	}
-	return a
-}
-
-// Linear interpolation between x and y. With a == 0 corresponding to x and
-// a == 1 corresponding to y.
-func Mix(x, y, a float64) float64 {
-	return x*(1-a) + y*a
 }
