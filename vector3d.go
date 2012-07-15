61000ae9e46357d4154dd1e207db86658ae73d64
Robert Williamson <williamson.robert@gmail.com>
2012-07-14 23:41:59 -0500

    Added some functions for 3D vectors.

--- vector3d.go
+++ vector3d.go
@@ -1,5 +1,26 @@
 package geometry
 
+// 3D Vector.
 type Vector3D struct {
 	X, Y, Z float64
+}
+
+// Returns the dot product of two vectors.
+func DotProduct3D(v1, v2 Vector3D) float64 {
+	return v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
+}
+
+// Returns the cross product of two vectors.
+func CrossProduct3D(v1, v2 Vector3D) Vector3D {
+	return Vector3D{v1.Y*v2.Z - v1.Z*v2.Y, v1.Z*v2.X - v1.X*v2.Z, v1.X*v2.Y - v1.Y*v2.X}
+}
+
+// Returns true if the two vectors are the same.
+func (v1 Vector3D) Equal(v2 Vector3D) bool {
+	return v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z
+}
+
+// Returns true if the two vectors are close.
+func (v1 Vector3D) FuzzyEqual(v2 Vector3D) bool {
+	return FuzzyEqual(v1.X, v2.X) && FuzzyEqual(v1.Y, v2.Y) && FuzzyEqual(v1.Z, v2.Z)
 }
--- vector3d_test.go
+++ vector3d_test.go
@@ -1,1 +1,36 @@
 package geometry
+
+import (
+	"testing"
+)
+
+func TestDotProduct3D(t *testing.T) {
+	if DotProduct3D(Vector3D{1, 2, 3}, Vector3D{4, 5, 6}) != 32 {
+		t.Error("DotProduct3D")
+	}
+}
+
+func TestCrossProduct3D(t *testing.T) {
+	if !CrossProduct3D(Vector3D{1, 2, 3}, Vector3D{4, 5, 6}).Equal(Vector3D{-3, 6, -3}) {
+		t.Error("CrossProduct3D")
+	}
+}
+
+func TestVector3DFuzzyEqual(t *testing.T) {
+	v1 := Vector3D{1.0, 1.0, 1.0}
+	v2 := v1
+	v2.X += 0.0000000000001
+	if v1.Equal(v2) {
+		t.Error("Vector3D.Equal")
+	}
+	if !v1.FuzzyEqual(v2) {
+		t.Error("Vector3D.FuzzyEqual")
+	}
+	v2.Y += 0.000000000001
+	if v1.Equal(v2) {
+		t.Error("Vector3D.Equal")
+	}
+	if v1.FuzzyEqual(v2) {
+		t.Error("Vector3D.FuzzyEqual")
+	}
+}
