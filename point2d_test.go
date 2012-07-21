e4ad18c11b34d85fc71ffa22a3e863ceb08478c2
Robert Williamson <williamson.robert@gmail.com>
2012-07-21 12:04:37 -0500

    Added benchmarks for point timings.

--- point2d_test.go
+++ point2d_test.go
@@ -4,11 +4,44 @@ import (
 	"testing"
 )
 
+func TestPoint2DCore(t *testing.T) {
+	p := &Point2D{0, 0}
+	p.Add(&Point2D{2, 1})
+	if !p.Equal(&Point2D{2, 1}) {
+		t.Error("Point2D.Add")
+	}
+	p.Subtract(&Point2D{1, 1})
+	if !p.Equal(&Point2D{1, 0}) {
+		t.Error("Point2D.Subtract")
+	}
+}
+
+func Benchmark_Point2D_Add(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.Add(p2)
+	}
+}
+
+func Benchmark_Point2D_Subtract(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.Subtract(p2)
+	}
+}
+
 func TestPointDistance2D(t *testing.T) {
 	p1 := &Point2D{-2, 1}
 	p2 := &Point2D{1, 5}
 	if p1.DistanceTo(p2) != 5 {
 		t.Error("Point2D.DistanceTo")
+	}
+}
+
+func Benchmark_Point2D_Distance(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.DistanceTo(p2)
 	}
 }
 
@@ -20,15 +53,10 @@ func TestPointDistanceSquared2D(t *testing.T) {
 	}
 }
 
-func TestPoint2DCore(t *testing.T) {
-	p := &Point2D{0, 0}
-	p.Add(&Point2D{2, 1})
-	if !p.Equal(&Point2D{2, 1}) {
-		t.Error("Point2D.Add")
-	}
-	p.Subtract(&Point2D{1, 1})
-	if !p.Equal(&Point2D{1, 0}) {
-		t.Error("Point2D.Subtract")
+func Benchmark_Point2D_SquaredDistance(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.SquaredDistanceTo(p2)
 	}
 }
 
@@ -51,5 +79,26 @@ func TestPoint2DFuzzyEqual(t *testing.T) {
 	}
 	if p1.FuzzyEqual(p2) {
 		t.Error("Point2D.FuzzyEqual")
+	}
+}
+
+func Benchmark_Point2D_Equal_1(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.Equal(p2)
+	}
+}
+
+func Benchmark_Point2D_Equal_2(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{1, 1}
+	for i := 0; i < b.N; i++ {
+		p1.Equal(p2)
+	}
+}
+
+func Benchmark_Point2D_FuzzyEqual(b *testing.B) {
+	p1, p2 := &Point2D{1, 1}, &Point2D{2, 1}
+	for i := 0; i < b.N; i++ {
+		p1.FuzzyEqual(p2)
 	}
 }
