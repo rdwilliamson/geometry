c71c7d7dbdcf080000ab1c6961c90fb1c314c1cf
Robert Williamson <williamson.robert@gmail.com>
2012-07-21 15:41:43 -0500

    Converted vector3d over, need to benchmark now.

--- vector3d.go
+++ vector3d.go
@@ -9,77 +9,37 @@ type Vector3D struct {
 	X, Y, Z float64
 }
 
-// Returns the dot product of two vectors.
-func DotProduct3D(v1, v2 Vector3D) float64 {
-	return v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
-}
-
-// Returns the cross product of two vectors.
-func CrossProduct3D(v1, v2 Vector3D) Vector3D {
-	return Vector3D{v1.Y*v2.Z - v1.Z*v2.Y, v1.Z*v2.X - v1.X*v2.Z, v1.X*v2.Y - v1.Y*v2.X}
-}
-
-// Returns true if the two vectors are the same.
-func (v1 Vector3D) Equal(v2 Vector3D) bool {
-	return v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z
-}
-
-// Returns true if the two vectors are close.
-func (v1 Vector3D) FuzzyEqual(v2 Vector3D) bool {
-	return FuzzyEqual(v1.X, v2.X) && FuzzyEqual(v1.Y, v2.Y) && FuzzyEqual(v1.Z, v2.Z)
-}
-
-// Returns the result of vector addition.
-func (v1 Vector3D) Plus(v2 Vector3D) Vector3D {
-	return Vector3D{v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z}
+// Returns a copy of the vector.
+func (v *Vector3D) Copy() Vector3D {
+	return *v
 }
 
 // Add a vector.
-func (v1 *Vector3D) Add(v2 Vector3D) {
+func (v1 *Vector3D) Add(v2 *Vector3D) {
 	v1.X += v2.X
 	v1.Y += v2.Y
 	v1.Z += v2.Z
 }
 
-// Returns the result of vector subtraction.
-func (v1 Vector3D) Minus(v2 Vector3D) Vector3D {
-	return Vector3D{v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z}
-}
-
 // Subtract a vector.
-func (v1 *Vector3D) Subtract(v2 Vector3D) {
+func (v1 *Vector3D) Subtract(v2 *Vector3D) {
 	v1.X -= v2.X
 	v1.Y -= v2.Y
 	v1.Z -= v2.Z
 }
 
-// Returns the element by element multiplication.
-func (v1 Vector3D) Times(v2 Vector3D) Vector3D {
-	return Vector3D{v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z}
-}
-
 // Multiplies the vector element by element.
-func (v1 *Vector3D) Multiply(v2 Vector3D) {
+func (v1 *Vector3D) Multiply(v2 *Vector3D) {
 	v1.X *= v2.X
 	v1.Y *= v2.X
 	v1.Z *= v2.Z
 }
 
-// Returns the element by element division.
-func (v1 Vector3D) Divided(v2 Vector3D) Vector3D {
-	return Vector3D{v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z}
-}
-
 // Divides the vector element by element.
-func (v1 *Vector3D) Divide(v2 Vector3D) {
+func (v1 *Vector3D) Divide(v2 *Vector3D) {
 	v1.X /= v2.X
 	v1.Y /= v2.Y
 	v1.Z /= v2.Z
-}
-
-// Returns the scaled vector.
-func (v Vector3D) Scaled(s float64) Vector3D {
-	return Vector3D{v.X * s, v.Y * s, v.Z * s}
 }
 
 // Scales the vector.
@@ -90,22 +50,33 @@ func (v *Vector3D) Scale(s float64) {
 }
 
 // Returns the length of the vector.
-func (v Vector3D) Length() float64 {
+func (v *Vector3D) Length() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
 }
 
 // Returns the squared length of the vector.
-func (v Vector3D) LengthSquared() float64 {
+func (v *Vector3D) LengthSquared() float64 {
 	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
 }
 
-// Returns a normalized vector. The zero vector remains unchanged.
-func (v Vector3D) Normalized() Vector3D {
-	if v.X == 0 && v.Y == 0 && v.Z == 0 {
-		return v
-	}
-	l := 1 / math.Sqrt(v.X*v.X+v.Y*v.Y+v.Z*v.Z)
-	return Vector3D{v.X * l, v.Y * l, v.Z * l}
+// Returns the dot product of two vectors.
+func DotProduct3D(v1, v2 *Vector3D) float64 {
+	return v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
+}
+
+// Returns the cross product of two vectors.
+func CrossProduct3D(v1, v2 *Vector3D) Vector3D {
+	return Vector3D{v1.Y*v2.Z - v1.Z*v2.Y, v1.Z*v2.X - v1.X*v2.Z, v1.X*v2.Y - v1.Y*v2.X}
+}
+
+// Returns true if the two vectors are the same.
+func (v1 *Vector3D) Equal(v2 *Vector3D) bool {
+	return v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z
+}
+
+// Returns true if the two vectors are close.
+func (v1 *Vector3D) FuzzyEqual(v2 *Vector3D) bool {
+	return FuzzyEqual(v1.X, v2.X) && FuzzyEqual(v1.Y, v2.Y) && FuzzyEqual(v1.Z, v2.Z)
 }
 
 // Normalizes the vector. The zero vector remains unchanged.
@@ -120,18 +91,18 @@ func (v *Vector3D) Normalize() {
 }
 
 // Returns the scalar projection.
-func (v1 Vector3D) ScalarProjectionOnto(v2 Vector3D) float64 {
+func (v1 *Vector3D) ScalarProjectionOnto(v2 *Vector3D) float64 {
 	return (v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z) / math.Sqrt(v2.X*v2.X+v2.Y*v2.Y+v2.Z*v2.Z)
 }
 
 // Returns the vector projection.
-func (v1 Vector3D) VectorProjectionOnto(v2 Vector3D) Vector3D {
+func (v1 *Vector3D) ProjectedOnto(v2 *Vector3D) Vector3D {
 	s := (v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z) / (v2.X*v2.X + v2.Y*v2.Y + v2.Z*v2.Z)
 	return Vector3D{v2.X * s, v2.Y * s, v2.Z * s}
 }
 
 // Returns the angle between two vectors.
-func (v1 Vector3D) AngleBetween(v2 Vector3D) float64 {
+func (v1 *Vector3D) AngleBetween(v2 *Vector3D) float64 {
 	dot := v1.X*v2.X + v1.Y*v2.Y + v1.Z*v2.Z
 	v1l := math.Sqrt(v1.X*v1.X + v1.Y*v1.Y + v1.Z*v1.Z)
 	v2l := math.Sqrt(v2.X*v2.X + v2.Y*v2.Y + v2.Z*v2.Z)
--- vector3d_test.go
+++ vector3d_test.go
@@ -5,21 +5,41 @@ import (
 	"testing"
 )
 
+func TestVector3DCore(t *testing.T) {
+	v := Vector3D{0, 0, 0}
+	v.Add(&Vector3D{3, 2, 1})
+	if !v.Equal(&Vector3D{3, 2, 1}) {
+		t.Error("Vector3D.Add")
+	}
+	v.Subtract(&Vector3D{2, 1, 0})
+	if !v.Equal(&Vector3D{1, 1, 1}) {
+		t.Error("Vector3D.Subtract")
+	}
+	v.Multiply(&Vector3D{0.5, 0.5, 0.5})
+	if !v.Equal(&Vector3D{0.5, 0.5, 0.5}) {
+		t.Error("Vector3D.Multiply")
+	}
+	v.Divide(&Vector3D{2, 2, 2})
+	if !v.Equal(&Vector3D{0.25, 0.25, 0.25}) {
+		t.Error("Vector3D.Divide")
+	}
+}
+
 func TestDotProduct3D(t *testing.T) {
-	if DotProduct3D(Vector3D{1, 2, 3}, Vector3D{4, 5, 6}) != 32 {
+	if DotProduct3D(&Vector3D{1, 2, 3}, &Vector3D{4, 5, 6}) != 32 {
 		t.Error("DotProduct3D")
 	}
 }
 
 func TestCrossProduct3D(t *testing.T) {
-	if !CrossProduct3D(Vector3D{1, 2, 3}, Vector3D{4, 5, 6}).Equal(Vector3D{-3, 6, -3}) {
+	if v := CrossProduct3D(&Vector3D{1, 2, 3}, &Vector3D{4, 5, 6}); !v.Equal(&Vector3D{-3, 6, -3}) {
 		t.Error("CrossProduct3D")
 	}
 }
 
 func TestVector3DFuzzyEqual(t *testing.T) {
-	v1 := Vector3D{1.0, 1.0, 1.0}
-	v2 := v1
+	v1 := &Vector3D{1.0, 1.0, 1.0}
+	v2 := &Vector3D{1.0, 1.0, 1.0}
 	v2.X += 0.0000000000001
 	if v1.Equal(v2) {
 		t.Error("Vector3D.Equal")
@@ -36,50 +56,10 @@ func TestVector3DFuzzyEqual(t *testing.T) {
 	}
 }
 
-func TestVector3DCore(t *testing.T) {
-	v := Vector3D{0, 0, 0}
-	v = v.Plus(Vector3D{1, 2, 3})
-	if !v.Equal(Vector3D{1, 2, 3}) {
-		t.Error("Vector3D.Plus")
-	}
-	v.Add(Vector3D{3, 2, 1})
-	if !v.Equal(Vector3D{4, 4, 4}) {
-		t.Error("Vector3D.Add")
-	}
-	v = v.Minus(Vector3D{1, 2, 3})
-	if !v.Equal(Vector3D{3, 2, 1}) {
-		t.Error("Vector3D.Minus")
-	}
-	v.Subtract(Vector3D{2, 1, 0})
-	if !v.Equal(Vector3D{1, 1, 1}) {
-		t.Error("Vector3D.Subtract")
-	}
-	v = v.Times(Vector3D{2, 2, 2})
-	if !v.Equal(Vector3D{2, 2, 2}) {
-		t.Error("Vector3D.Times")
-	}
-	v.Multiply(Vector3D{0.5, 0.5, 0.5})
-	if !v.Equal(Vector3D{1, 1, 1}) {
-		t.Error("Vector3D.Multiply")
-	}
-	v = v.Divided(Vector3D{0.5, 0.5, 0.5})
-	if !v.Equal(Vector3D{2, 2, 2}) {
-		t.Error("Vector3D.Divided")
-	}
-	v.Divide(Vector3D{2, 2, 2})
-	if !v.Equal(Vector3D{1, 1, 1}) {
-		t.Error("Vector3D.Divide")
-	}
-}
-
 func TestVector3DScale(t *testing.T) {
-	v := Vector3D{1, 1, 1}
-	v = v.Scaled(2)
-	if !v.Equal(Vector3D{2, 2, 2}) {
-		t.Error("Vector3D.Scaled")
-	}
+	v := &Vector3D{1, 1, 1}
 	v.Scale(0.5)
-	if !v.Equal(Vector3D{1, 1, 1}) {
+	if !v.Equal(&Vector3D{0.5, 0.5, 0.5}) {
 		t.Error("Vector3D.Scale")
 	}
 }
@@ -96,44 +76,36 @@ func TestVector3DLength(t *testing.T) {
 
 func TestVector3DNormalize(t *testing.T) {
 	v := Vector3D{-6, 3, -1}
-	v = v.Normalized()
-	if !v.Equal(Vector3D{-6 / math.Sqrt(46), 3 / math.Sqrt(46), -1 / math.Sqrt(46)}) {
-		t.Error("Vector3D.Normalized")
-	}
-	v = Vector3D{-6, 3, -1}
 	v.Normalize()
-	if !v.Equal(Vector3D{-6 / math.Sqrt(46), 3 / math.Sqrt(46), -1 / math.Sqrt(46)}) {
+	if !v.Equal(&Vector3D{-6 / math.Sqrt(46), 3 / math.Sqrt(46), -1 / math.Sqrt(46)}) {
 		t.Error("Vector3D.Normalize")
 	}
 	v = Vector3D{0, 0, 0}
-	if !v.Normalized().Equal(Vector3D{0, 0, 0}) {
-		t.Error("Vector3D.Normalized")
-	}
 	v.Normalize()
-	if !v.Equal(Vector3D{0, 0, 0}) {
+	if !v.Equal(&Vector3D{0, 0, 0}) {
 		t.Error("Vector3D.Normalize")
 	}
 }
 
 func TestScalarProjectionOnto3D(t *testing.T) {
-	v1 := Vector3D{-3, 2, -4}
-	v2 := Vector3D{2, -5, 1}
+	v1 := &Vector3D{-3, 2, -4}
+	v2 := &Vector3D{2, -5, 1}
 	if v1.ScalarProjectionOnto(v2) != -20/math.Sqrt(30) {
 		t.Error("Vector3D.ScalarProjectionOnto")
 	}
 }
 
 func TestVectorProjectionOnto3D(t *testing.T) {
-	v1 := Vector3D{-3, 2, -4}
-	v2 := Vector3D{2, -5, 1}
-	if !v1.VectorProjectionOnto(v2).FuzzyEqual(Vector3D{-4.0 / 3.0, 10.0 / 3.0, -2.0 / 3.0}) {
+	v1 := &Vector3D{-3, 2, -4}
+	v2 := &Vector3D{2, -5, 1}
+	if v1.ProjectedOnto(v2); v1.FuzzyEqual(&Vector3D{-4.0 / 3.0, 10.0 / 3.0, -2.0 / 3.0}) {
 		t.Error("Vector3D.VectorProjectionOnto")
 	}
 }
 
 func TestAngleBetween3D(t *testing.T) {
-	v1 := Vector3D{1, 0, 0}
-	v2 := Vector3D{1, 1, 0}
+	v1 := &Vector3D{1, 0, 0}
+	v2 := &Vector3D{1, 1, 0}
 	if !FuzzyEqual(v1.AngleBetween(v2), math.Pi/4) {
 		t.Error("Vector3D.AngleBetween")
 	}
