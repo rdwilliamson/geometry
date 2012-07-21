08f64f6b783e6a0531fc4cc78484b3507394ddca
Robert Williamson <williamson.robert@gmail.com>
2012-07-21 15:29:52 -0500

    Converted over 3D points.

--- point3d.go
+++ point3d.go
@@ -9,48 +9,44 @@ type Point3D struct {
 	X, Y, Z float64
 }
 
-// Returns the addition of two points.
-func (p1 Point3D) Plus(p2 Point3D) Point3D {
-	return Point3D{p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z}
+// Returns a copy of the point.
+func (p *Point3D) Copy() Point3D {
+	return *p
 }
 
 // Add a point.
-func (p1 *Point3D) Add(p2 Point3D) {
+func (p1 *Point3D) Add(p2 *Point3D) {
 	p1.X += p2.X
 	p1.Y += p2.Y
 	p1.Z += p2.Z
 }
 
-// Returns the difference between the first and second point.
-func (p1 Point3D) Minus(p2 Point3D) Point3D {
-	return Point3D{p1.X - p2.X, p1.Y - p2.Y, p1.Z - p2.Z}
-}
-
 // Subtract a point.
-func (p1 *Point3D) Subtract(p2 Point3D) {
+func (p1 *Point3D) Subtract(p2 *Point3D) {
 	p1.X -= p2.X
 	p1.Y -= p2.Y
 	p1.Z -= p2.Z
 }
 
 // Returns true if the two points are the same.
-func (p1 Point3D) Equal(p2 Point3D) bool {
+func (p1 *Point3D) Equal(p2 *Point3D) bool {
 	return p1.X == p2.X && p1.Y == p2.Y && p1.Z == p2.Z
 }
 
 // Returns true if the two points are very close.
-func (p1 Point3D) FuzzyEqual(p2 Point3D) bool {
-	return FuzzyEqual(p1.X, p2.X) && FuzzyEqual(p1.Y, p2.Y) && FuzzyEqual(p1.Z, p2.Z)
+func (p1 *Point3D) FuzzyEqual(p2 *Point3D) bool {
+	dx, dy, dz := p2.X-p1.X, p2.Y-p1.Y, p2.Z-p1.Z
+	return dx*dx+dy*dy+dz*dz < 0.000000000001*0.000000000001
 }
 
 // Returns the distance between the two points.
-func (p1 Point3D) DistanceTo(p2 Point3D) float64 {
+func (p1 *Point3D) DistanceTo(p2 *Point3D) float64 {
 	dx, dy, dz := p2.X-p1.X, p2.Y-p1.Y, p2.Z-p1.Z
 	return math.Sqrt(dx*dx + dy*dy + dz*dz)
 }
 
 // Returns the squared distance between the two points.
-func (p1 Point3D) SquaredDistanceTo(p2 Point3D) float64 {
+func (p1 *Point3D) SquaredDistanceTo(p2 *Point3D) float64 {
 	dx, dy, dz := p2.X-p1.X, p2.Y-p1.Y, p2.Z-p1.Z
 	return dx*dx + dy*dy + dz*dz
 }
--- point3d_test.go
+++ point3d_test.go
@@ -5,45 +5,37 @@ import (
 	"testing"
 )
 
-func TestPointDistance3D(t *testing.T) {
-	p1 := Point3D{1, 7, -3}
-	p2 := Point3D{-5, 4, -2}
-	if p1.DistanceTo(p2) != math.Sqrt(46) {
-		t.Error("Point3D.DistanceTo")
-	}
-}
-
-func TestPointDistanceSquared3D(t *testing.T) {
-	p1 := Point3D{1, 7, -3}
-	p2 := Point3D{-5, 4, -2}
-	if p1.SquaredDistanceTo(p2) != 46 {
-		t.Error("Point3D.SquaredDistanceTo")
-	}
-}
-
 func TestPoint3DCore(t *testing.T) {
 	p := Point3D{0, 0, 0}
-	p = p.Plus(Point3D{1, 2, 3})
-	if !p.Equal(Point3D{1, 2, 3}) {
-		t.Error("Point3D.Plus")
-	}
-	p.Add(Point3D{3, 2, 1})
-	if !p.Equal(Point3D{4, 4, 4}) {
+	p.Add(&Point3D{3, 2, 1})
+	if !p.Equal(&Point3D{3, 2, 1}) {
 		t.Error("Point3D.Add")
 	}
-	p = p.Minus(Point3D{1, 1, 1})
-	if !p.Equal(Point3D{3, 3, 3}) {
-		t.Error("Point3D.Minus")
-	}
-	p.Subtract(Point3D{1, 1, 1})
-	if !p.Equal(Point3D{2, 2, 2}) {
+	p.Subtract(&Point3D{1, 1, 1})
+	if !p.Equal(&Point3D{2, 1, 0}) {
 		t.Error("Point3D.Subtract")
 	}
 }
 
+func Benchmark_Point3D_Add(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{3, 4, 5}
+	for i := 0; i < b.N; i++ {
+		p1.Add(p2)
+	}
+}
+
+func Benchmark_Point3D_Subtract(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{3, 4, 5}
+	for i := 0; i < b.N; i++ {
+		p1.Subtract(p2)
+	}
+}
+
 func TestPoint3DFuzzyEqual(t *testing.T) {
-	p1 := Point3D{1.0, 1.0, 1.0}
-	p2 := p1
+	p1 := &Point3D{1.0, 1.0, 1.0}
+	p2 := &Point3D{1.0, 1.0, 1.0}
 	p2.X += 0.0000000000001
 	if p1.Equal(p2) {
 		t.Error("Point3D.Equal")
@@ -57,5 +49,69 @@ func TestPoint3DFuzzyEqual(t *testing.T) {
 	}
 	if p1.FuzzyEqual(p2) {
 		t.Error("Point3D.FuzzyEqual")
+	}
+}
+
+func Benchmark_Point3D_Equal_1(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{0, 1, 2}
+	for i := 0; i < b.N; i++ {
+		p1.Equal(p2)
+	}
+}
+
+func Benchmark_Point3D_Equal_2(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{0, 1, 3}
+	for i := 0; i < b.N; i++ {
+		p1.Equal(p2)
+	}
+}
+
+func Benchmark_Point3D_Equal_3(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{0, 3, 3}
+	for i := 0; i < b.N; i++ {
+		p1.Equal(p2)
+	}
+}
+
+func Benchmark_Point3D_FuzzyEqual(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{0, 1, 2}
+	for i := 0; i < b.N; i++ {
+		p1.FuzzyEqual(p2)
+	}
+}
+
+func TestPointDistance3D(t *testing.T) {
+	p1 := &Point3D{1, 7, -3}
+	p2 := &Point3D{-5, 4, -2}
+	if p1.DistanceTo(p2) != math.Sqrt(46) {
+		t.Error("Point3D.DistanceTo")
+	}
+}
+
+func Benchmark_Point3D_Distance(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{1, 2, 3}
+	for i := 0; i < b.N; i++ {
+		p1.DistanceTo(p2)
+	}
+}
+
+func TestPointDistanceSquared3D(t *testing.T) {
+	p1 := &Point3D{1, 7, -3}
+	p2 := &Point3D{-5, 4, -2}
+	if p1.SquaredDistanceTo(p2) != 46 {
+		t.Error("Point3D.SquaredDistanceTo")
+	}
+}
+
+func Benchmark_Point3D_SquaredDistanceTo(b *testing.B) {
+	p1 := &Point3D{0, 1, 2}
+	p2 := &Point3D{1, 2, 3}
+	for i := 0; i < b.N; i++ {
+		p1.SquaredDistanceTo(p2)
 	}
 }
