a9d8ca5631e0407a551c0ca35de863abb1e102db
Robert Williamson <williamson.robert@gmail.com>
2012-08-16 21:32:21 -0500

    Changed variable names.

--- distance2d.go
+++ distance2d.go
@@ -8,10 +8,10 @@ import (
 // to rotate about its midpoint to pass through point b.
 func Distance2DLinePointAngular(a *Line2D, b *Vector2D) float64 {
 	mpx, mpy := (a.P1.X+a.P2.X)*0.5, (a.P1.Y+a.P2.Y)*0.5
-	l1dx, l1dy := a.P1.X-mpx, a.P1.Y-mpy
-	l2dx, l2dy := b.X-mpx, b.Y-mpy
-	return math.Abs(math.Acos((l1dx*l2dx+l1dy*l2dy)/
-		math.Sqrt((l1dx*l1dx+l1dy*l1dy)*(l2dx*l2dx+l2dy*l2dy))) - math.Pi/2)
+	adx, ady := a.P1.X-mpx, a.P1.Y-mpy
+	ldx, ldy := b.X-mpx, b.Y-mpy
+	return math.Abs(math.Acos((adx*ldx+ady*ldy)/
+		math.Sqrt((adx*adx+ady*ady)*(ldx*ldx+ldy*ldy))) - math.Pi/2)
 }
 
 // Distance2DLinePointAngularCosSquared returns the cos of the squared angle
@@ -19,18 +19,18 @@ func Distance2DLinePointAngular(a *Line2D, b *Vector2D) float64 {
 // point b.
 func Distance2DLinePointAngularCosSquared(a *Line2D, b *Vector2D) float64 {
 	mpx, mpy := (a.P1.X+a.P2.X)*0.5, (a.P1.Y+a.P2.Y)*0.5
-	l1dx, l1dy := a.P1.X-mpx, a.P1.Y-mpy
-	l2dx, l2dy := b.X-mpx, b.Y-mpy
-	dot := l1dx*l2dx + l1dy*l2dy
-	return dot * dot / ((l1dx*l1dx + l1dy*l1dy) * (l2dx*l2dx + l2dy*l2dy))
+	adx, ady := a.P1.X-mpx, a.P1.Y-mpy
+	ldx, ldy := b.X-mpx, b.Y-mpy
+	dot := adx*ldx + ady*ldy
+	return dot * dot / ((adx*adx + ady*ady) * (ldx*ldx + ldy*ldy))
 }
 
 // Distance2DLinePoint returns the distance point b is from line a.
 func Distance2DLinePoint(a *Line2D, b *Vector2D) float64 {
 	// http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/
-	ldx, ldy := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
-	u := (ldx*(b.X-a.P1.X) + ldy*(b.Y-a.P1.Y)) / (ldx*ldx + ldy*ldy)
-	x, y := b.X-(a.P1.X+ldx*u), b.Y-(a.P1.Y+ldy*u)
+	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
+	u := (adx*(b.X-a.P1.X) + ady*(b.Y-a.P1.Y)) / (adx*adx + ady*ady)
+	x, y := b.X-(a.P1.X+adx*u), b.Y-(a.P1.Y+ady*u)
 	return math.Sqrt(x*x + y*y)
 }
 
@@ -38,9 +38,9 @@ func Distance2DLinePoint(a *Line2D, b *Vector2D) float64 {
 // a.
 func Distance2DLinePointSquared(a *Line2D, b *Vector2D) float64 {
 	// http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/
-	ldx, ldy := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
-	u := (ldx*(b.X-a.P1.X) + ldy*(b.Y-a.P1.Y)) / (ldx*ldx + ldy*ldy)
-	x, y := b.X-(a.P1.X+ldx*u), b.Y-(a.P1.Y+ldy*u)
+	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
+	u := (adx*(b.X-a.P1.X) + ady*(b.Y-a.P1.Y)) / (adx*adx + ady*ady)
+	x, y := b.X-(a.P1.X+adx*u), b.Y-(a.P1.Y+ady*u)
 	return x*x + y*y
 }
 
@@ -48,19 +48,19 @@ func Distance2DLinePointSquared(a *Line2D, b *Vector2D) float64 {
 // point b.
 func Distance2DLineSegmentPoint(a *Line2D, b *Vector2D) float64 {
 	// http://softsurfer.com/Archive/algorithm_0102/algorithm_0102.htm
-	ldx, ldy := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
-	c1 := ldx*(b.X-a.P1.X) + ldy*(b.Y-a.P1.Y)
+	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
+	c1 := adx*(b.X-a.P1.X) + ady*(b.Y-a.P1.Y)
 	if c1 <= 0 {
 		x, y := b.X-a.P1.X, b.Y-a.P1.Y
 		return math.Sqrt(x*x + y*y)
 	}
-	c2 := ldx*ldx + ldy*ldy
+	c2 := adx*adx + ady*ady
 	if c2 <= c1 {
 		x, y := b.X-a.P2.X, b.Y-a.P2.Y
 		return math.Sqrt(x*x + y*y)
 	}
 	c1 /= c2
-	x, y := b.X-(a.P1.X+ldx*c1), b.Y-(a.P1.Y+ldy*c1)
+	x, y := b.X-(a.P1.X+adx*c1), b.Y-(a.P1.Y+ady*c1)
 	return math.Sqrt(x*x + y*y)
 }
 
@@ -68,19 +68,19 @@ func Distance2DLineSegmentPoint(a *Line2D, b *Vector2D) float64 {
 // segment a and point b.
 func Distance2DLineSegmentPointSquared(a *Line2D, b *Vector2D) float64 {
 	// http://softsurfer.com/Archive/algorithm_0102/algorithm_0102.htm
-	ldx, ldy := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
-	c1 := ldx*(b.X-a.P1.X) + ldy*(b.Y-a.P1.Y)
+	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
+	c1 := adx*(b.X-a.P1.X) + ady*(b.Y-a.P1.Y)
 	if c1 <= 0 {
 		x, y := b.X-a.P1.X, b.Y-a.P1.Y
 		return x*x + y*y
 	}
-	c2 := ldx*ldx + ldy*ldy
+	c2 := adx*adx + ady*ady
 	if c2 <= c1 {
 		x, y := b.X-a.P2.X, b.Y-a.P2.Y
 		return x*x + y*y
 	}
 	c1 /= c2
-	x, y := b.X-(a.P1.X+ldx*c1), b.Y-(a.P1.Y+ldy*c1)
+	x, y := b.X-(a.P1.X+adx*c1), b.Y-(a.P1.Y+ady*c1)
 	return x*x + y*y
 }
 
