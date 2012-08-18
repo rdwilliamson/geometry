5e30b85566267e711bdbce750bbe25c9d6985e13
Robert Williamson <williamson.robert@gmail.com>
2012-08-18 13:48:40 -0500

    Have a plan for how to deal with edge cases of intersections shown with 2d line line intersections.

--- intersection2d.go
+++ intersection2d.go
@@ -1,35 +1,5 @@
 package geometry
 
-// Fuzzy line line intersection is almost twice as slow so leave the regular
-// for when user knows they aren't parallel. Since equality with floating point
-// numbers is rare is there even a point of checking exact equality, or just
-// let it return infinite numbers.
-
-// Intersection2DLineLine sets point z to the intersection of a and b then
-// returns the number of intersections.
-//
-// Possible return values are:
-// -1 if the lines are coincident, z is untouched.
-// 0 if the lines are parallel, z is untouched.
-// 1 otherwise, z is set to the intersection of the two lines.
-func Intersection2DLineLine(a, b *Line2D, z *Vector2D) int {
-	// http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
-	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
-	bdx, bdy := b.P2.X-b.P1.X, b.P2.Y-b.P1.Y
-	d := bdy*adx - bdx*ady
-	if d == 0 {
-		am, bm := ady/adx, bdy/bdx
-		if a.P1.Y-am*a.P1.X == b.P1.Y-bm*b.P1.X {
-			return -1
-		}
-		return 0
-	}
-	ua := (bdx*(a.P1.Y-b.P1.Y) - bdy*(a.P1.X-b.P1.X)) / d
-	z.X = a.P1.X + ua*adx
-	z.Y = a.P1.Y + ua*ady
-	return 1
-}
-
 // Intersection2DFuzzyLineLine sets point z to the intersection of a and b then
 // returns the number of intersections.
 //
@@ -53,6 +23,18 @@ func Intersection2DFuzzyLineLine(a, b *Line2D, z *Vector2D) int {
 	z.X = a.P1.X + ua*adx
 	z.Y = a.P1.Y + ua*ady
 	return 1
+}
+
+// Intersection2DLineLine sets point z to the intersection of a and b then
+// returns z.
+func Intersection2DLineLine(a, b *Line2D, z *Vector2D) *Vector2D {
+	// http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+	adx, ady := a.P2.X-a.P1.X, a.P2.Y-a.P1.Y
+	bdx, bdy := b.P2.X-b.P1.X, b.P2.Y-b.P1.Y
+	ua := (bdx*(a.P1.Y-b.P1.Y) - bdy*(a.P1.X-b.P1.X)) / (bdy*adx - bdx*ady)
+	z.X = a.P1.X + ua*adx
+	z.Y = a.P1.Y + ua*ady
+	return z
 }
 
 // Intersection2DLineSegmentLineSegment determines the intersection of two line
--- intersection2d_test.go
+++ intersection2d_test.go
@@ -1,16 +1,34 @@
 package geometry
 
 import (
+	"math"
 	"testing"
 )
 
-type intersection2DLineLineData struct {
+func (v *Vector2D) toPositiveInf() *Vector2D {
+	if math.IsInf(v.X, -1) {
+		v.X = math.Inf(1)
+	}
+	if math.IsInf(v.Y, -1) {
+		v.Y = math.Inf(1)
+	}
+	return v
+}
+
+func (a *Vector2D) nanEqual(b *Vector2D) bool {
+	if math.IsNaN(a.X) && math.IsNaN(a.Y) && math.IsNaN(b.X) && math.IsNaN(b.Y) {
+		return true
+	}
+	return a.Equal(b)
+}
+
+type intersection2DFuzzyLineLineData struct {
 	l1, l2 Line2D
 	p      Vector2D
 	n      int
 }
 
-var intersection2DLineLineValues = []intersection2DLineLineData{
+var intersection2DFuzzyLineLineValues = []intersection2DFuzzyLineLineData{
 	{Line2D{Vector2D{0, 0}, Vector2D{1, 1}},
 		Line2D{Vector2D{0, 1}, Vector2D{1, 0}},
 		Vector2D{0.5, 0.5}, 1},
@@ -25,64 +43,37 @@ var intersection2DLineLineValues = []intersection2DLineLineData{
 		Vector2D{}, -1},
 }
 
-func testVector2DFromLineIntersection(d intersection2DLineLineData, t *testing.T) {
+func testIntersection2DFuzzyLineLine(d intersection2DFuzzyLineLineData, t *testing.T) {
 	var p Vector2D
-	if n := Intersection2DLineLine(&d.l1, &d.l2, &p); n != d.n ||
+	if n := Intersection2DFuzzyLineLine(&d.l1, &d.l2, &p); n != d.n ||
 		(d.n == 1 && !p.Equal(&d.p)) {
 		if d.n == 1 {
-			t.Error("Vector2D.FromLineIntersection", d.l1, d.l2, "want", d.n,
+			t.Error("Intersection2D.FuzzyLineLine", d.l1, d.l2, "want", d.n,
 				d.p, "got", n, p)
 		} else {
-			t.Error("Vector2D.FromLineIntersection", d.l1, d.l2, "want", d.n,
+			t.Error("Intersection2D.FuzzyLineLine", d.l1, d.l2, "want", d.n,
 				"got", n)
 		}
 	}
-	if n := Intersection2DLineLine(&d.l2, &d.l1, &p); n != d.n ||
+	if n := Intersection2DFuzzyLineLine(&d.l2, &d.l1, &p); n != d.n ||
 		(d.n == 1 && !p.Equal(&d.p)) {
 		if d.n == 1 {
-			t.Error("Vector2D.FromLineIntersection", d.l2, d.l1, "want", d.n,
+			t.Error("Intersection2D.FuzzyLineLine", d.l2, d.l1, "want", d.n,
 				d.p, "got", n, p)
 		} else {
-			t.Error("Vector2D.FromLineIntersection", d.l2, d.l1, "want", d.n,
+			t.Error("Intersection2D.FuzzyLineLine", d.l2, d.l1, "want", d.n,
 				"got", n)
 		}
 	}
 }
 
-func TestIntersection2DLineLine(t *testing.T) {
-	// for _, v := range intersection2DLineLineValues {
-	// 	testVector2DFromLineIntersection(v, t)
-	// 	v.l1.P1, v.l1.P2 = v.l1.P2, v.l1.P1
-	// 	testVector2DFromLineIntersection(v, t)
-	// 	v.l2.P1, v.l2.P2 = v.l2.P2, v.l2.P1
-	// 	testVector2DFromLineIntersection(v, t)
-	// }
-}
-
-func Benchmark_Intersection2D_LineLine_Coincident(b *testing.B) {
-	l1 := &Line2D{Vector2D{0, 0}, Vector2D{1, 1}}
-	l2 := &Line2D{Vector2D{-1, -1}, Vector2D{0, 0}}
-	p := &Vector2D{}
-	for i := 0; i < b.N; i++ {
-		Intersection2DLineLine(l1, l2, p)
-	}
-}
-
-func Benchmark_Intersection2D_LineLine_Parallel(b *testing.B) {
-	l1 := &Line2D{Vector2D{0, 0}, Vector2D{1, 1}}
-	l2 := &Line2D{Vector2D{1, 0}, Vector2D{2, 1}}
-	p := &Vector2D{}
-	for i := 0; i < b.N; i++ {
-		Intersection2DLineLine(l1, l2, p)
-	}
-}
-
-func Benchmark_Intersection2D_LineLine_Intersect(b *testing.B) {
-	l1 := &Line2D{Vector2D{0, 0}, Vector2D{1, 1}}
-	l2 := &Line2D{Vector2D{0, 1}, Vector2D{1, 0}}
-	p := &Vector2D{}
-	for i := 0; i < b.N; i++ {
-		Intersection2DLineLine(l1, l2, p)
+func TestIntersection2DFuzzyLineLine(t *testing.T) {
+	for _, v := range intersection2DFuzzyLineLineValues {
+		testIntersection2DFuzzyLineLine(v, t)
+		v.l1.P1, v.l1.P2 = v.l1.P2, v.l1.P1
+		testIntersection2DFuzzyLineLine(v, t)
+		v.l2.P1, v.l2.P2 = v.l2.P2, v.l2.P1
+		testIntersection2DFuzzyLineLine(v, t)
 	}
 }
 
@@ -110,6 +101,55 @@ func Benchmark_Intersection2DFuzzy_LineLine_Intersect(b *testing.B) {
 	p := &Vector2D{}
 	for i := 0; i < b.N; i++ {
 		Intersection2DFuzzyLineLine(l1, l2, p)
+	}
+}
+
+type intersection2DLineLineData struct {
+	l1, l2 Line2D
+	p      Vector2D
+}
+
+var intersection2DLineLineValues = []intersection2DLineLineData{
+	{Line2D{Vector2D{0, 0}, Vector2D{1, 1}},
+		Line2D{Vector2D{0, 1}, Vector2D{1, 0}},
+		Vector2D{0.5, 0.5}},
+	{Line2D{Vector2D{0, 0}, Vector2D{-1, -1}},
+		Line2D{Vector2D{0, 1}, Vector2D{1, 0}},
+		Vector2D{0.5, 0.5}},
+	{Line2D{Vector2D{0, 0}, Vector2D{1, 1}},
+		Line2D{Vector2D{1, 0}, Vector2D{2, 1}},
+		Vector2D{math.Inf(1), math.Inf(1)}},
+	{Line2D{Vector2D{0, 0}, Vector2D{1, 1}},
+		Line2D{Vector2D{-2, -2}, Vector2D{-1, -1}},
+		Vector2D{math.NaN(), math.NaN()}},
+}
+
+func testIntersection2DLineLine(d intersection2DLineLineData, t *testing.T) {
+	var p Vector2D
+	if !Intersection2DLineLine(&d.l1, &d.l2, &p).toPositiveInf().nanEqual(&d.p) {
+		t.Error("Intersection2D.LineLine", d.l1, d.l2, "want", d.p, "got", p)
+	}
+	if !Intersection2DLineLine(&d.l2, &d.l1, &p).toPositiveInf().nanEqual(&d.p) {
+		t.Error("Intersection2D.LineLine", d.l2, d.l1, "want", d.p, "got", p)
+	}
+}
+
+func TestIntersection2DLineLine(t *testing.T) {
+	for _, v := range intersection2DLineLineValues {
+		testIntersection2DLineLine(v, t)
+		v.l1.P1, v.l1.P2 = v.l1.P2, v.l1.P1
+		testIntersection2DLineLine(v, t)
+		v.l2.P1, v.l2.P2 = v.l2.P2, v.l2.P1
+		testIntersection2DLineLine(v, t)
+	}
+}
+
+func Benchmark_Intersection2D_LineLine(b *testing.B) {
+	l1 := &Line2D{Vector2D{0, 0}, Vector2D{1, 1}}
+	l2 := &Line2D{Vector2D{0, 1}, Vector2D{1, 0}}
+	p := &Vector2D{}
+	for i := 0; i < b.N; i++ {
+		Intersection2DLineLine(l1, l2, p)
 	}
 }
 
